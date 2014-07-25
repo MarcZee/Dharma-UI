976260e27200873156592dba37101704eb1b309b
mziegerath <mziegerath@AKM3-WS2275.AKM3.local>
2014-07-25 14:00:11 +0200

    Init

---
+++ _bak/cp_local_storage.js
@@ -0,0 +1,41 @@
+	
+	"use strict";
+	
+	var CPLocalStorage={
+		init:function(){
+			this._clearLocalStorageForEveryPageButEditorPages();
+		},
+		_clearLocalStorageForEveryPageButEditorPages:function(){
+			var e=document.location.pathname,
+				t=e.match(/\/(live|pen|collab|professor|details|pres|bye|boomerang|write)/);
+				
+			t||this.clear();
+		},
+		clear:function(){
+			this._exec(function(){
+				localStorage.clear();
+			});
+		},
+		setItem:function(e,t){
+			this._exec(function(){
+				localStorage.setItem(e,t);
+			});
+		},
+		getItem:function(e){
+			return this._exec(function(){
+				return localStorage.getItem(e);
+			});
+		},
+		removeItem:function(e){
+			this._exec(function(){
+				localStorage.removeItem(e);
+			});
+		},
+		_exec:function(e){
+			try{
+				return e();
+			}catch(t){ }
+		}
+	};
+			
+	CPLocalStorage.init();
